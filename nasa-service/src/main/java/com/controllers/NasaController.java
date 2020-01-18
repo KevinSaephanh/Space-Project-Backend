f6acd8df5388d765947f06ba376f854afa5848c5
Kevin <Kevin_Saephanh@yahoo.com>
2020-01-18 15:09:58 -0600

    Added nasa-service as discovery client

--- nasa-service/src/main/java/com/controllers/NasaController.java
+++ nasa-service/src/main/java/com/controllers/NasaController.java
@@ -9,21 +9,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/nasa")
 public class NasaController {
-	public static final String NASA_KEY = "${NASA_KEY}";
+	public static final String NASA_KEY = System.getenv("NASA_KEY");
 	public static final String BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
 
-	private RestTemplate restTemplate;
-
+	RestTemplate restTemplate = new RestTemplate();
+	
 	@ResponseStatus(HttpStatus.OK)
 	@GetMapping(value = "/apod", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ObjectNode getPicOfTheDay() {
+	public String getPicOfTheDay() {
 		final String apod_url = "https://api.nasa.gov/planetary/apod?api_key=";
-		return restTemplate.getForObject(apod_url + NASA_KEY, ObjectNode.class);
+		return restTemplate.getForObject(apod_url + NASA_KEY, String.class);
 	}
 }
